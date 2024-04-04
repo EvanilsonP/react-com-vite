20b5e30db3c06b8796208e67ed95a148339f5275
Evanilson Pereira <pevanilson99@gmail.com>
2024-04-04 18:09:39 -0300

    JSX, Váriaveis, funções e objetos

--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -1,11 +1,22 @@
+const getWeekDay = () => {
+    return new Intl.DateTimeFormat('pt-BR', { weekday: 'long'} ).format(new Date());
+};
+
 export function Person() {
+    const data = {
+        name: 'Elon Musk',
+        avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkHMB5ECYwHvgPcVehN-eLB6BGl3Nif2u15A9D1Qv9CA&s',
+        roles: ['CEO da Tesla', 'CEO da SpaceX']
+    };
+
     return (
         <div>
-            <h1>Elon Musk</h1>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkHMB5ECYwHvgPcVehN-eLB6BGl3Nif2u15A9D1Qv9CA&s" alt="Elon Musk" className="w-32"/>
+            <h1>{data.name} + {getWeekDay()}</h1>
+            <img src={data.avatar} alt={data.name} />
+
             <ul>
-                <li>CEO da Tesla</li>
-                <li>CEO da SpaceX</li>
+                <li>{data.roles[0]}</li>
+                <li>{data.roles[1]}</li>
             </ul>
         </div>
     );
